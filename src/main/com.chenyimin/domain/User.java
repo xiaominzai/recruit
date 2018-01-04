a6147543d641aee78f444747498c857c06ec4f7f
JyingLee <853281682@qq.com>
2018-01-04 09:26:53 +0800

    user表增加属性

--- src/main/com.chenyimin/domain/User.java
+++ src/main/com.chenyimin/domain/User.java
@@ -9,6 +9,16 @@ public class User  implements Serializable{
     private String password;
     private String lastIp;
     private Date lastVisit;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String sex;
+    private String dob;
+    private String email;
+    private String country;
+    private String work_time;
+    private String jiaoyu;
+    private String self_me;
 
     public int getUserId() {
         return userId;
@@ -48,5 +58,85 @@ public class User  implements Serializable{
 
     public void setLastVisit(Date lastVisit) {
         this.lastVisit = lastVisit;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getWork_time() {
+        return work_time;
+    }
+
+    public void setWork_time(String work_time) {
+        this.work_time = work_time;
+    }
+
+    public String getJiaoyu() {
+        return jiaoyu;
+    }
+
+    public void setJiaoyu(String jiaoyu) {
+        this.jiaoyu = jiaoyu;
+    }
+
+    public String getSelf_me() {
+        return self_me;
+    }
+
+    public void setSelf_me(String self_me) {
+        this.self_me = self_me;
     }
 }
--- src/main/com.chenyimin/web/LoginController.java
+++ src/main/com.chenyimin/web/LoginController.java
@@ -5,6 +5,7 @@ import domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import service.UserService;
 
@@ -19,11 +20,20 @@ public class LoginController {
 
     @RequestMapping(value = "index.html")
     public String loginPage(){
-        return "login";
+        return "index";
+    }
+    @RequestMapping(value = "resume.html")
+    public String resumePage(){
+        return "resume";
+    }
+    @RequestMapping(value = "register.html")
+    public String registerPage(){
+        return "register";
     }
 
     @RequestMapping(value = "loginCheck.html")
     public ModelAndView loginCheck(HttpServletRequest request, LoginCommand loginCommand){
+//        request.getParameter("")
         boolean isValidUser = userService.hasMatchUser(loginCommand.getUserName(), loginCommand.getPassword());
         if (!isValidUser){
             return new ModelAndView("login","error","用户名或密码错误");
